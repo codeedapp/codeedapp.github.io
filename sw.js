e3519c461f754cebae6e5b4f18c575e3674f769f
codeedapp <47481306+codeedapp@users.noreply.github.com>
2019-04-03 11:43:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // I'm a new service worker
 self.addEventListener('install', event => {
   console.log('Service worker installing...');
-  // Add a call to skipWaiting here
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
