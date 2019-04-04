4c375879a604b33666d5b2f4274ffb35b3129c83
codeedapp <47481306+codeedapp@users.noreply.github.com>
2019-04-04 17:37:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,29 +8,10 @@ self.addEventListener('activate', event => {
   console.log('Service worker activating...');
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method === 'GET') {
-    let url = event.request.url.indexOf(self.location.origin) !== -1 ?
-      event.request.url.split(`${self.location.origin}/`)[1] :
-      event.request.url;
-    let isFileCached = $FILES.indexOf(url) !== -1;
-
-    if (isFileCached) {
-      event.respondWith(
-        caches.open($CACHE_STORE)
-          .then(cache => {
-            return cache.match(url)
-              .then(response => {
-                if (response) {
-                  return response;
-                }
-                throw Error('There is not response for such request', url);
-              });
-          })
-          .catch(error => {
-            return fetch(event.request);
-          })
-      );
-    }
-  }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
